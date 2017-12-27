47571afabd1675253ebe1f32b3d75010a9be737a
zhangzhen <zhangzhen2@hongte.info>
2017-12-27 21:13:43 +0800

    接口参数更新

---
+++ risk-drools/src/main/java/com/ht/risk/service/impl/ruleaction/ExcuteTotalScoreServiceImpl.java
@@ -0,0 +1,43 @@
+package com.ht.risk.service.impl.ruleaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.ht.risk.model.fact.RuleExecutionObject;
+import com.ht.risk.model.fact.RuleExecutionResult;
+import com.ht.risk.service.DroolsActionService;
+
+@Service
+public class ExcuteTotalScoreServiceImpl implements DroolsActionService{
+	
+	private Logger log = LoggerFactory.getLogger(ExcuteTotalScoreServiceImpl.class);
+	
+	
+    @Override
+    public void execute(RuleExecutionObject fact, RuleExecutionResult result) {
+    	log.info("########统计方法开始");
+    	Object totalObj = fact.getGlobalMap().get("total");
+    	Integer total = 0 ; 
+    	if(totalObj != null){
+    		total =  Integer.parseInt(String.valueOf(totalObj));
+    	}else{
+    		total = 0;
+    	}
+    	Object scoreObj = result.getMap().get("score");
+    	if(scoreObj != null){
+    		total += Integer.parseInt(String.valueOf(scoreObj));
+    	}
+    	fact.getGlobalMap().put("total", total);
+    	log.info("########统计当前结果："+total);
+    	log.info("########统计方法结束");
+    }
+
+	@Override
+	public void statisResult(RuleExecutionResult result) {
+		
+	}
+
+    
+
+}
---
+++ risk-drools/src/main/java/com/ht/risk/service/impl/ruleaction/IntopiecesServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ht.risk.service.impl.ruleaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.ht.risk.model.fact.RuleExecutionObject;
+import com.ht.risk.model.fact.RuleExecutionResult;
+import com.ht.risk.service.DroolsActionService;
+
+@Service
+public class IntopiecesServiceImpl implements DroolsActionService{
+	
+	private Logger log = LoggerFactory.getLogger(IntopiecesServiceImpl.class);
+	
+    @Override
+    public void execute(RuleExecutionObject fact, RuleExecutionResult result) {
+    	log.info("########进件方法开始执行");
+    	String flag = String.valueOf(result.getMap().get("flag"));
+    	if("是".equals(flag)){
+    		fact.getGlobalMap().put("into_pieces_flag", "0");
+    		log.info("########规则判定结果为：进件");
+    	}else{
+    		fact.getGlobalMap().put("into_pieces_flag", "1");
+    		log.info("########规则判定结果为：不进件");
+    	}
+    	log.info("########进件方法执行结束");
+    }
+
+	@Override
+	public void statisResult(RuleExecutionResult result) {
+		
+	}
+
+
+}
