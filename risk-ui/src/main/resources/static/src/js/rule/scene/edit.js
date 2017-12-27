40ddd5b4e4dd0e4b0296d34277301613a3803d4e
zhangpeng <zhangpeng@TD-PC>
2017-12-27 14:18:12 +0800

    添加评分卡

--- risk-ui/src/main/resources/static/src/js/rule/scene/edit.js
+++ risk-ui/src/main/resources/static/src/js/rule/scene/edit.js
@@ -413,13 +413,16 @@ function getRuleList(){
             //结果
             else if(i < headLen-1){
                 var actionV = $(e).find("a.actionVal").attr("data-value");
+
+                var actionType = $(e).find("a.actionType").attr("data-value");
+
                 if(actionV == ''  ){
                     layer.msg('必选项不能为空');
                     return null;
                 }
                var actionValInfo = {
                     //动作id参数Id
-                    actionParamId:'3',
+                    actionParamId:actionType,
                     //值
                     paramValue:actionV
                 };
