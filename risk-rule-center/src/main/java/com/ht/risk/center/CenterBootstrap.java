a0d7af32f2279c600a8b52b669e50944b184425a
zhangzhen <zhangzhen2@hongte.info>
2018-01-10 09:13:33 +0800

    注册中心启动类删除多余注解

--- risk-rule-center/src/main/java/com/ht/risk/center/CenterBootstrap.java
+++ risk-rule-center/src/main/java/com/ht/risk/center/CenterBootstrap.java
@@ -14,7 +14,6 @@ import org.apache.commons.logging.LogFactory;
  */
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-@EnableZuulProxy
 public class CenterBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(CenterBootstrap.class, args);
