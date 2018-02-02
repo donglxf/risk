667fce0076762d18569ce07deb27f5ccbe376cae
zhangzhen <zhangzhen2@hongte.info>
2018-02-02 12:00:33 +0800

    模型验证结果查询接口

---
+++ risk-ui/src/main/java/com/ht/risk/ui/controller/RuleSupervisionController.java
@@ -0,0 +1,51 @@
+package com.ht.risk.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/supervision")
+public class RuleSupervisionController {
+
+	/**
+	 * 日志查询首页
+	* @Title: unDeployList
+	* @Description: 决策变量绑定首页
+	* @param @return    设定文件
+	* @return String    返回类型
+	* @throws
+	 */
+    @RequestMapping(value = "/log/index",method = RequestMethod.GET)
+    public String logIndex(){
+        return "supervision/logindex";
+    }
+
+    /**
+     * 模型报告首页
+     * @Title: unDeployList
+     * @Description: 决策变量绑定首页
+     * @param @return    设定文件
+     * @return String    返回类型
+     * @throws
+     */
+    @RequestMapping(value = "/model/report",method = RequestMethod.GET)
+    public String modelReport(){
+        return "supervision/modelReport";
+    }
+
+    /**
+     * 策略报告首页
+    * @Title: deployList
+    * @Description: 决策验证首页
+    * @param @return    设定文件
+    * @return String    返回类型
+    * @throws
+     */
+    @RequestMapping(value = "/rule/report",method = RequestMethod.GET)
+    public String deployList(){
+        return "supervision/ruleReport";
+    }
+
+
+}
