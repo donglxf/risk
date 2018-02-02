11175e7d4e562aa6347555a8dfe6d1b892ba7ef7
dongyoubing <dongyoubing@hongte.info>
2018-02-02 17:06:45 +0800

    代码优化

---
+++ risk-drools/src/main/java/com/ht/risk/mapper/RuleActionVersionMapper.java
@@ -0,0 +1,16 @@
+package com.ht.risk.mapper;
+
+import com.ht.risk.common.mapper.SuperMapper;
+import com.ht.risk.model.RuleActionVersion;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author dyb
+ * @since 2018-02-02
+ */
+public interface RuleActionVersionMapper extends SuperMapper<RuleActionVersion> {
+
+}
---
+++ risk-drools/src/main/java/com/ht/risk/service/RuleActionVersionService.java
@@ -0,0 +1,16 @@
+package com.ht.risk.service;
+
+import com.ht.risk.common.service.BaseService;
+import com.ht.risk.model.RuleActionVersion;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author dyb
+ * @since 2018-02-02
+ */
+public interface RuleActionVersionService extends BaseService<RuleActionVersion> {
+
+}
