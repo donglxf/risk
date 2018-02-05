07de60976734e8d76ed969d7bb6a442529f53a81
huang <huangzengmeng@hongte.info>
2018-02-05 17:50:11 +0800

    对接完成天秤接口: 1.手机号验证 2.负面信息查询

--- risk-api/src/main/java/com/ht/risk/api/feign/eip/TcRpc.java
+++ risk-api/src/main/java/com/ht/risk/api/feign/eip/TcRpc.java
@@ -42,8 +42,25 @@ public interface TcRpc {
     @PostMapping(value = "/black/self", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
     Result<SelfDtoOut> self(@RequestBody OldLaiIn input);
 
+    /**
+     * 描述：手机号验证
+     * @param input
+     * @return a
+     * @autor 黄增猛
+     * @date 2018/2/5 9:38
+     */
+    @PostMapping(value = "/black/mobileValid", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    Result<MobileValidDtoOut> mobileValid(@RequestBody MobileValidDtoIn input);
 
-
+    /**
+     * 描述：负面消息查询
+     * @param input
+     * @return a
+     * @autor 黄增猛
+     * @date 2018/2/5 9:38
+     */
+    @PostMapping(value = "/news/negativeSearch", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    Result<NegativeSearchDtoOut> negativeSearch(@RequestBody NegativeSearchDtoIn input);
 
 
 }
--- risk-eip/src/main/java/com/ht/risk/eip/controller/TcController.java
+++ risk-eip/src/main/java/com/ht/risk/eip/controller/TcController.java
@@ -1,10 +1,7 @@
 package com.ht.risk.eip.controller;
 
 import com.ht.risk.api.feign.eip.TcRpc;
-import com.ht.risk.api.model.eip.NetLoanIn;
-import com.ht.risk.api.model.eip.NetLoanOut;
-import com.ht.risk.api.model.eip.OldLaiIn;
-import com.ht.risk.api.model.eip.OldLaiOut;
+import com.ht.risk.api.model.eip.*;
 import com.ht.ussp.core.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -36,6 +33,7 @@ public class TcController {
         Result<NetLoanOut> result =  tcRpc.netLoan(input);
         return result;
     }
+
     @PostMapping("/black/oldLai")
     @ApiOperation(value = "老赖黑名单",httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
     public Result<OldLaiOut> oldLai(OldLaiIn input) throws Exception{
@@ -43,6 +41,19 @@ public class TcController {
         return result;
     }
 
+    @PostMapping("/black/mobileValid")
+    @ApiOperation(value = "手机号验证",httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Result<MobileValidDtoOut> oldLai(MobileValidDtoIn input) throws Exception{
+        Result<MobileValidDtoOut> result =  tcRpc.mobileValid(input);
+        return result;
+    }
+
+    @PostMapping("/news/negativeSearch")
+    @ApiOperation(value = "负面消息查询",httpMethod = "POST", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Result<NegativeSearchDtoOut> negativeSearch(NegativeSearchDtoIn input) throws Exception{
+        Result<NegativeSearchDtoOut> result =  tcRpc.negativeSearch(input);
+        return result;
+    }
 
 
 
