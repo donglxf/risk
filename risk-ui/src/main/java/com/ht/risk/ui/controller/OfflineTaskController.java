f7c7aacb764138757b40d4fa66cb20cb6fcbbc58
liuzhengquan@hongte.info <liu@2018>
2018-02-05 12:41:22 +0800

    离线任务

---
+++ risk-ui/src/main/java/com/ht/risk/ui/controller/OfflineTaskController.java
@@ -0,0 +1,33 @@
+package com.ht.risk.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+* @ClassName: OffLineTaskController
+* @Description: 离线任务配置管理
+* @author liuzq
+* @date 
+* 
+*/
+@Controller
+@RequestMapping("/rule")
+public class OfflineTaskController {
+    @RequestMapping(value = "/offlineTask/model",method = RequestMethod.GET)
+    public String ruleConstant(){
+    	return "rule/offlineTask/rule_offline_task";
+    }
+    
+    @RequestMapping(value = "/offlinetask/edit",method = RequestMethod.GET)
+    public String ruleConstantEdit(){
+    	return "rule/constant/rule_constant_edit";
+    }
+    
+    @RequestMapping(value = "/offlinetask/itemEdit",method = RequestMethod.GET)
+    public String ruleConstantItemEdit(){
+    	return "rule/constant/rule_constant_item_edit";
+    }
+
+}
