85d43bea621c9d1b931e732e9de32ecaa0503e35
zhangzhen <zhangzhen2@hongte.info>
2018-02-26 16:00:38 +0800

    数据库脚本整理

--- risk-activiti/src/main/java/com/ht/risk/activiti/service/task/impl/HourseRuleResultImpl.java
+++ risk-activiti/src/main/java/com/ht/risk/activiti/service/task/impl/HourseRuleResultImpl.java
@@ -1,13 +1,14 @@
 package com.ht.risk.activiti.service.task.impl;
 
 import com.ht.risk.activiti.service.task.HourseRuleDataGain;
+import com.ht.risk.activiti.service.task.HourseRuleResult;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service("hourseRuleDataGain")
-public class HourseRuleResultImpl implements HourseRuleDataGain {
+@Service("hourseRuleResult")
+public class HourseRuleResultImpl implements HourseRuleResult {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(HourseRuleResultImpl.class);
 
