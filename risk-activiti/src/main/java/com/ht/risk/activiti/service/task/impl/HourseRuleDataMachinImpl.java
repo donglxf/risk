dd936a51b5b68f716f99ab3a18a3ad67cf8eac6f
zhangzhen <zhangzhen2@hongte.info>
2018-02-27 15:25:31 +0800

    房贷模型修改

--- risk-activiti/src/main/java/com/ht/risk/activiti/service/task/impl/HourseRuleDataMachinImpl.java
+++ risk-activiti/src/main/java/com/ht/risk/activiti/service/task/impl/HourseRuleDataMachinImpl.java
@@ -7,19 +7,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * 数据加工接口
+ */
 @Service("hourseRuleDataMachin")
 public class HourseRuleDataMachinImpl implements HourseRuleDataMachin {
 
     protected static final Logger LOGGER = LoggerFactory.getLogger(HourseRuleDataMachinImpl.class);
 
     /**
-     *
+     * 数据加工
      * @param execution
      * @throws Exception
      */
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         LOGGER.error("HourseRuleDataMachinImpl execute method excute start...");
+        execution.setVariable("flag","001");
         LOGGER.error("HourseRuleDataMachinImpl execute method excute end...");
     }
 }
