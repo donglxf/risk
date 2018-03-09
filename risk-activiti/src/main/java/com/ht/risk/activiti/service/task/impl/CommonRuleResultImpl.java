5a070b15f8d2c386ea83addc52e976d4937881e0
zhangzhen <zhangzhen2@hongte.info>
2018-03-09 14:55:45 +0800

    模型验证bug修改

---
+++ risk-activiti/src/main/java/com/ht/risk/activiti/service/task/impl/CommonRuleResultImpl.java
@@ -0,0 +1,16 @@
+package com.ht.risk.activiti.service.task.impl;
+
+import com.ht.risk.activiti.service.task.CommonRuleResult;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+/**
+ *  结果处理获取接口
+ */
+public class CommonRuleResultImpl implements CommonRuleResult {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+    }
+}
