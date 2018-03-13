87c1965dd0c6b72f7ed1a6ea4f868fc520a855d8
zhangzhen <zhangzhen2@hongte.info>
2018-03-13 17:19:02 +0800

    模型名称修改

--- risk-activiti-extend/src/main/java/com/ht/risk/activiti/mapper/ActProcReleaseMapper.java
+++ risk-activiti-extend/src/main/java/com/ht/risk/activiti/mapper/ActProcReleaseMapper.java
@@ -3,6 +3,9 @@ package com.ht.risk.activiti.mapper;
 import com.ht.risk.activiti.entity.ActProcRelease;
 import com.ht.risk.common.mapper.SuperMapper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>
  *  Mapper 接口
@@ -12,5 +15,7 @@ import com.ht.risk.common.mapper.SuperMapper;
  * @since 2018-01-16
  */
 public interface ActProcReleaseMapper extends SuperMapper<ActProcRelease> {
+
+    public List<ActProcRelease> getModelLastedVersion(Map<String,Object> paramter);
 
 }
