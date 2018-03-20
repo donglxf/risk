a225fa3c4118dc6f51801e64a46057a0ddfb7b79
zhangzhen <zhangzhen2@hongte.info>
2018-03-20 18:29:34 +0800

    远程调用配置修改

--- risk-eip/src/main/java/com/ht/risk/eip/config/MultipartSupportConfig.java
+++ risk-eip/src/main/java/com/ht/risk/eip/config/MultipartSupportConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+//@Configuration
 public class MultipartSupportConfig {
     @Bean
     @Primary
